3b087a5df50ab359c4a6ec32e46f86d5100c7080
Nigam-g <ayushnig020@gmail.com>
2020-07-02 00:19:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,211 @@
+window.onload=function(){
+    document.getElementById("btn").addEventListener("click",
+function(){
+    var a=document.getElementById("front");
+    a.style.display="none";
+    var b=document.getElementById("relquz");
+    b.style.display="block";
+})
+    
+    document.getElementById("b1").addEventListener("click",
+function(){
+    alert("Right Answer");
+})
+    document.getElementById("b2").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-A) June 21");
+
+})
+    document.getElementById("b3").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-A) June 21");
+
+})
+    document.getElementById("b4").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-A) June 21");
+
+})
+    document.getElementById("b5").addEventListener("click",
+function(){
+    alert("Right Answer");
+
+})
+    document.getElementById("b6").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-A) Jan 15");
+
+})
+    document.getElementById("b7").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-A) Jan 15");
+
+})
+    document.getElementById("b8").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-A) Jan 15");
+
+})
+
+    document.getElementById("b9").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-C) September 27");
+
+})
+    document.getElementById("b10").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-C) September 27");
+
+})
+    document.getElementById("b11").addEventListener("click",
+function(){
+    alert("Right Answer");
+
+})
+    document.getElementById("b12").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-C) September 27");
+
+})
+    document.getElementById("b13").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-B) Electrol Photo Identity Cards(EPIC)");
+
+})
+    document.getElementById("b14").addEventListener("click",
+function(){
+    alert("Right Answer");
+
+})
+    document.getElementById("b15").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-B) Electrol Photo Identity Cards(EPIC)");
+
+})
+    document.getElementById("b16").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-B) Electrol Photo Identity Cards(EPIC)");
+
+})
+    document.getElementById("b17").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-B) Shakti Sthal");
+
+})
+    document.getElementById("b18").addEventListener("click",
+function(){
+    alert("Right Answer");
+
+})
+    document.getElementById("b19").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-B) Shakti Sthal");
+
+})
+    document.getElementById("b20").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-B) Shakti Sthal");
+
+})
+    document.getElementById("b21").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-C) Ghandi Jayanti");
+
+})
+    document.getElementById("b22").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-C) Ghandi Jayanti");
+
+})
+    document.getElementById("b23").addEventListener("click",
+function(){
+    alert("Right Answer");
+
+})
+    document.getElementById("b24").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-C) Ghandi Jayanti");
+
+})
+    document.getElementById("b25").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-C) Sunderlal Bahuguna");
+
+})
+    document.getElementById("b26").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-C) Sunderlal Bahuguna");
+
+})
+    document.getElementById("b27").addEventListener("click",
+function(){
+    alert("Right Answer");
+
+})
+    document.getElementById("b28").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-C) Sunderlal Bahuguna");
+
+})
+    document.getElementById("b29").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-B) Water Coservation");
+
+})
+    document.getElementById("b30").addEventListener("click",
+function(){
+    alert("Right Answer");
+
+})
+    document.getElementById("b31").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-B) Water Coservation");
+
+})
+    document.getElementById("b32").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-B) Water Coservation");
+
+})
+    document.getElementById("b33").addEventListener("click",
+function(){
+    alert("Right Answer");
+
+})
+    document.getElementById("b34").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-A) New Delhi");
+
+})
+    document.getElementById("b35").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-A) New Delhi");
+
+})
+    document.getElementById("b36").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-A) New Delhi");
+
+})
+    document.getElementById("b37").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-D) 20th November");
+
+})
+    document.getElementById("b38").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-D) 20th November");
+
+})
+    document.getElementById("b39").addEventListener("click",
+function(){
+    alert("Wrong Answer!!Coreect Answer:-D) 20th November");
+
+})
+    document.getElementById("b40").addEventListener("click",
+function(){
+    alert("Right Answer");
+
+})
+
+};
